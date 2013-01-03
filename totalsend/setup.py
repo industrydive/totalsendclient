6e1db557da8bef5a7323bacf277af0a6b2c3e4d5
Miriam Sexton <miriam@industrydive.com>
2013-01-03 10:11:08 -0500

    setup info

---
+++ totalsend/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(
+    name='TotalSend',
+    version='0.0.1',
+    author='Eli Dickinson',
+    author_email='eli@industrydive.com',
+    packages=['totalsend'],
+    # scripts=['bin/stowe-towels.py','bin/wash-towels.py'],
+    # url='http://pypi.python.org/pypi/TowelStuff/',
+    # license='LICENSE.txt',
+    description='Using TotalSend API.',
+    # long_description=open('README.txt').read(),
+    # install_requires=[
+    #     "Django >= 1.1.1",
+    #     "caldav == 0.1.4",
+    # ],
+)
